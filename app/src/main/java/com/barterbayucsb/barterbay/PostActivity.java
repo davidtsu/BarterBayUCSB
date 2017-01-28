bbfadec25ba95fa7fa7b1db5692f8421cbf6fbcf
Daniel Ben-Naim <Daniel Ben-Naim>
2017-01-27 17:30:16 -0800

    edited floating action button and created a post activity

---
+++ app/src/main/java/com/barterbayucsb/barterbay/PostActivity.java
@@ -0,0 +1,15 @@
+package com.barterbayucsb.barterbay;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by David on 1/27/2017.
+ */
+public class PostActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myoffers);
+    }
+}
