53c307931fdd3514bafca953429491e68dd8bd93
Daniel Ben-Naim <Daniel Ben-Naim>
2017-02-03 16:14:34 -0800

    some ui changes

--- app/src/main/java/com/barterbayucsb/barterbay/MainActivity.java
+++ app/src/main/java/com/barterbayucsb/barterbay/MainActivity.java
@@ -2,7 +2,7 @@ package com.barterbayucsb.barterbay;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -30,8 +30,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this,PostActivity.class);
+                startActivity(intent);
             }
         });
         local_button.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/barterbayucsb/barterbay/PostActivity.java
+++ app/src/main/java/com/barterbayucsb/barterbay/PostActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
- * Created by David on 1/27/2017.
+ * Created by Daniel on 1/27/2017.
  */
 public class PostActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.myoffers);
+        setContentView(R.layout.newpost);
     }
 }
