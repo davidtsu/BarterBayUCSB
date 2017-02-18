0c4406e24152f6e6a9a21c48c74c79761815b98f
dummyindex <dummy_index@163.com>
2017-02-17 17:24:15 -0800

    add test offer, test user, sample APIs to servergate class.

--- app/src/main/java/com/barterbayucsb/barterbay/Offer.java
+++ app/src/main/java/com/barterbayucsb/barterbay/Offer.java
@@ -1,5 +1,7 @@
 package com.barterbayucsb.barterbay;
 
+import java.util.ArrayList;
+
 /**
  * Created by Ming Chen on 2/7/2017.
  */
@@ -7,5 +9,11 @@ package com.barterbayucsb.barterbay;
 class Offer {
     private String name = "";
     private String depiction = "";
+    String id;
+    public Offer(){
+        name = "test user";
+        id = "test id";
+        depiction = "a test item";
+    }
 
 }
--- app/src/main/java/com/barterbayucsb/barterbay/ServerGate.java
+++ app/src/main/java/com/barterbayucsb/barterbay/ServerGate.java
@@ -8,9 +8,29 @@ class ServerGate {
     /*
     A class for handling communication with servers
      */
-
+    User test_user= new User();
+    Offer test_offer = new Offer();
     User login(String user_id, String user_password) {
         //TODO: implement login. null if login failed, an User instance if login success
-        return null;
+        return test_user;
     }
+
+    /*
+    retrieve user is a function to get user information from server.
+    it sends an http request to user
+     */
+    User retrieve_user(String  user_id){
+        //todo: implement this function
+        return test_user;
+    }
+
+    /*
+
+     */
+    Offer retrieve_offer(String offer_id){
+        return test_offer;
+    }
+
+    //todo: add more methods here for interacting with server if needed
+    //todo: params above like offer_id, user_id is not absoluitely needed, you can change it if need
 }
